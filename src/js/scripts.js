f36864e80afb4c0764d101366c5e4d9d2efd48ae
Jonas <login@jonas-brueggen.de>
2020-04-23 00:03:43 +0700

    Initial commit

---
+++ src/js/scripts.js
@@ -0,0 +1,72 @@
+function customPopup(content, maxWidth, link = null) {
+	if (link == null){
+		//use "content"
+		popupContainer = document.querySelector(".popup_container");
+		//cleanup
+		while (popupContainer.firstChild) {
+			popupContainer.removeChild(popupContainer.firstChild);
+		}
+		let popupElem = document.createElement("div");
+		popupElem.innerHTML = content;
+		popupElem.style.maxWidth=maxWidth;
+		popupContainer.appendChild(popupElem);
+		popupContainer.classList.add("active");
+		requestAnimationFrame(function(){
+			setTimeout(function(){popupElem.classList.add("move");},10);
+		});
+	} else {
+		//load and display link
+		let xhr = new XMLHttpRequest();
+
+		xhr.open('get', link);
+		xhr.onload = function() { customPopup(xhr.response, maxWidth); };
+		xhr.send();
+	}
+}
+
+function removeElementsByClass(className){
+	let elements = document.getElementsByClassName(className);
+	while(elements.length > 0){
+		elements[0].parentNode.removeChild(elements[0]);
+	}
+}
+
+function setRandomBackgrounds(className){
+	let classesBg = ["bg1","bg2","bg3","bg4","bg5"];
+	let classesBorder = ["border1","border2","border3","border4","border5"];
+	let elems = document.getElementsByClassName(className);
+	let prevNumber = -1;
+	let modNumbers1 = [-1,-1];
+	let modNumbers2 = [-1,-1,-1,-1];
+
+	for(let i = 0; i < elems.length; i++){
+		let rNumber = Math.floor(Math.random() * classesBg.length);
+		let evilNumber1 = prevNumber;
+		let evilNumber2 = modNumbers1[i%2];
+		let evilNumber3 = modNumbers2[i%4];
+		while (rNumber == evilNumber1 || rNumber == evilNumber2 || rNumber == evilNumber3) rNumber = Math.floor(Math.random() * classesBg.length);
+		
+		elems[i].classList.add(classesBg[rNumber], classesBorder[rNumber]);
+
+		prevNumber = rNumber;
+		modNumbers1[i%2] = rNumber;
+		modNumbers2[i%4] = rNumber;
+	}
+}
+
+function colorLogo(toColor = true) {
+	let elem = document.getElementById("id_logo_wrapper");
+	if (toColor) elem.classList.add("animate_in");
+	else elem.classList.remove("animate_in");
+}
+
+function triggerContactTileAnimation(){
+
+	let elem = document.getElementById("id_contact_tiles");
+	elem.classList.add("start_animation");
+	requestAnimationFrame(function(){
+		setTimeout(function(){
+			elem.classList.remove("start_animation");
+		},1100);
+	});
+}
